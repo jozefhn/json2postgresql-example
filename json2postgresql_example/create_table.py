93bf33e36b29449d1ebdfecaa13a06035b14baa8
jozefhn <jozef.hn@gmail.com>
2022-01-23 22:36:22 +0100

    add create sql table script

---
+++ json2postgresql_example/create_table.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import psycopg2
+from config import get_config
+
+
+def create_tables():
+    commands = (
+        ('CREATE TABLE interface ('
+            'id SERIAL PRIMARY KEY,'
+            'connection INTEGER,'
+            'name VARCHAR(255) NOT NULL,'
+            'description VARCHAR(255),'
+            'config json,'
+            'type VARCHAR(50),'
+            'infra_type VARCHAR(50),'
+            'port_channel_id INTEGER,'
+            'max_frame_size INTEGER)'),
+        )
+    conn = None
+    try:
+        params = get_config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        for command in commands:
+            cur.execute(command)
+
+        cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_tables()
